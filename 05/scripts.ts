535944a640abf7c50307648c1df24b64ac25b8ea
Eduarda Silva Santos <ettuarda@gmail.com>
2024-05-19 22:59:39 -0300

    ok

--- 05/scripts.ts
+++ 05/scripts.ts
@@ -2,8 +2,6 @@ const soletrar = (nome: string): string => {
     return nome.split('').join('-');
 };
 
-// Exemplo de uso da função
 const nomeExemplo = 'programador';
 const resulta = soletrar(nomeExemplo);
 console.log(resulta);
-// Saída esperada: "p-r-o-g-r-a-m-a-d-o-r"
